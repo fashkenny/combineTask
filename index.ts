2ac58d400e8285ce09d20d36fcb070b757e2acf5
unknown <fashkenny>
2023-08-07 14:15:19 +0100

    pushing from kennys laptop

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors"
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt" 
 
-const port:number = 4250
-const app = express()
+const server = "HELLO LIZZY"
